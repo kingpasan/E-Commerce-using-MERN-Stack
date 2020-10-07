f0eb32e626d1b7539fa474ace5632d3dd6ba5f99
Pasan Semage <pmsemage@gmail.com>
2020-10-07 15:03:13 +0530

    user authentication and user is a admin middleware added

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { requireSignin } = require('../controllers/auth');
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userByID } = require('../controllers/user');
 
 
-router.get("/secret/:userID", requireSignin, (req, res) => {
+router.get("/secret/:userID", requireSignin, isAuth, isAdmin, (req, res) => {
     res.json({
         user: req.profile
     });

4de233b49231fe66c3ba5dfd6db0f9000c5ab9e3
Pasan Semage <pmsemage@gmail.com>
2020-10-13 14:30:48 +0530

    category update, delete and get all categories functionalities added.

--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/category');
+const { create, categoryByID, read, update, remove, list } = require('../controllers/category');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userByID } = require('../controllers/user');
 
+router.get("/category/:categoryID", read);
 router.post("/category/create/:userID", requireSignin, isAuth, isAdmin, create);
+router.put("/category/:categoryID/:userID", requireSignin, isAuth, isAdmin, update);
+router.delete("/category/:categoryID/:userID", requireSignin, isAuth, isAdmin, remove);
+router.get("/categories", list);
 
+router.param('categoryID', categoryByID)
 router.param('userID', userByID);
 
 module.exports = router;

a1f4e5a285a6ee424891f044fe684472d2c2effc
Pasan Semage <pmsemage@gmail.com>
2020-10-13 13:43:07 +0530

    product remove and update funtionalities added.

--- controllers/product.js
+++ controllers/product.js
@@ -4,6 +4,20 @@ const fs = require("fs");
 const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+
+exports.productByID = (req, res, next, id) =>{
+    Product.findById(id).exec((err, product)=>{
+        if(err || !product){
+            return res.status(400).json({
+                error: 'Product not found!'
+            });
+        }
+
+        req.product = product;
+        next();
+    });
+};
+
 exports.create = (req, res) => {
 
     let form = new formidable.IncomingForm();
@@ -18,7 +32,7 @@ exports.create = (req, res) => {
         const { name, description, price, category, quantity, shipping } = field;
 
         //validation to product
-        if(!name || !description || !!price || !category || !quantity || !shipping){
+        if(!name || !description || !price || !category || !quantity || !shipping){
             return res.status(400).json({
                 error: 'All fields are required!'
             });
@@ -48,4 +62,79 @@ exports.create = (req, res) => {
             res.json(result);
         });
     });
-}
+};
+
+
+exports.read = (req, res) =>{
+
+    req.product.photo = undefined;
+
+    return res.json(req.product);
+
+};
+
+exports.remove = (req, res) => {
+    let product = req.product;
+
+    product.remove((err, deletedProduct)=>{
+        if(err)
+        {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+
+        res.json({
+            message: "Product deleted successfully!"
+        });
+    })
+};
+
+
+exports.update = (req, res) =>{
+
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, field, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            });
+        }
+
+        const { name, description, price, category, quantity, shipping } = field;
+
+        //validation to product
+        if(!name || !description || !price || !category || !quantity || !shipping){
+            return res.status(400).json({
+                error: 'All fields are required!'
+            });
+        }
+
+        let product = req.product;
+        product = _.extend(product, field);
+
+        if (files.photo) {
+
+            if (files.photo.size > 5000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 5mb in file size'
+                });
+            }
+
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+
+        product.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+
+            res.json(result);
+        });
+    });
+
+};
--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/product');
+const { create, productByID, read, remove, update } = require('../controllers/product');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userByID } = require('../controllers/user');
 
+router.get("/product/:productID", read);
 router.post("/product/create/:userID", requireSignin, isAuth, isAdmin, create);
+router.delete("/product/:productID/:userID", requireSignin, isAuth, isAdmin, remove);
+router.put("/product/:productID/:userID", requireSignin, isAuth, isAdmin, update);
 
 router.param('userID', userByID);
+router.param('productID', productByID);
 
 module.exports = router;

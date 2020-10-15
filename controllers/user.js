858a8ab2a1bdf4ea0586fc92f73b903b481c208a
Pasan Semage <pmsemage@gmail.com>
2020-10-15 18:05:36 +0530

    user profile and user details update functions added.

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,14 @@
 const User = require("../models/user");
+const { use } = require("../routes/user");
 
 
 exports.userByID = (req, res, next, id) => {
 
-    User.findById(id).exec((err, user)=> {
-        
-        if(err || !user){
+    User.findById(id).exec((err, user) => {
+
+        if (err || !user) {
             return res.status(400).json({
-                error : "User not found"
+                error: "User not found"
             });
         }
 
@@ -15,4 +16,28 @@ exports.userByID = (req, res, next, id) => {
         next();
 
     });
+};
+
+exports.read = (req, res) => {
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    return res.json(req.profile);
+};
+
+exports.update = (req, res) => {
+    User.findByIdAndUpdate(
+        { _id: req.profile._id },
+        { $set: req.body },
+        { new: true },
+        (err, user) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Unauthorized action."
+                });
+            }
+
+            user.hashed_password = undefined;
+            user.salt = undefined;
+            res.json(user);
+        });
 };
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
-const { userByID } = require('../controllers/user');
+const { userByID, read, update } = require('../controllers/user');
 
 
 router.get("/secret/:userID", requireSignin, isAuth, isAdmin, (req, res) => {
@@ -10,6 +10,10 @@ router.get("/secret/:userID", requireSignin, isAuth, isAdmin, (req, res) => {
         user: req.profile
     });
 });
+
+router.get("/user/:userID", requireSignin, isAuth, read);
+router.put("/user/:userID", requireSignin, isAuth, update);
+
 
 router.param('userID', userByID);
 
